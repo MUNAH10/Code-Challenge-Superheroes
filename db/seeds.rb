4af3e1749534ef908bf114434762602c77cab030
MUNAH10 <munahassan5075@gmail.com>
2023-03-26 00:34:00 +0300

    created migration & seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Seed data for heros table
+Hero.create(name: 'Clark Kent', super_name: 'Superman')
+Hero.create(name: 'Bruce Wayne', super_name: 'Batman')
+Hero.create(name: 'Peter Parker', super_name: 'Spiderman')
+
+# Seed data for powers table
+Power.create(name: 'Super Strength', description: 'Ability to lift heavy objects with ease')
+Power.create(name: 'Flight', description: 'Ability to fly')
+Power.create(name: 'Super Speed', description: 'Ability to move at superhuman speeds')
+Power.create(name: 'Web-slinging', description: 'Ability to shoot webs and swing through the city')
+
+# Seed data for hero_powers join table
+HeroPower.create(hero_id: 1, power_id: 1, strength: 'strong')
+HeroPower.create(hero_id: 1, power_id: 2, strength: 'average')
+HeroPower.create(hero_id: 1, power_id: 3, strength: 'strong')
+HeroPower.create(hero_id: 2, power_id: 1, strength: 'weak')
+HeroPower.create(hero_id: 2, power_id: 4, strength: 'average')
+HeroPower.create(hero_id: 3, power_id: 2, strength: 'average')
+HeroPower.create(hero_id: 3, power_id: 3, strength: 'strong')
+HeroPower.create(hero_id: 3, power_id: 4, strength: 'strong')
