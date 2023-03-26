18f9834c99952a522b802eb7e4238ca213be147f
MUNAH10 <munahassan5075@gmail.com>
2023-03-27 01:47:05 +0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-
+  resources :heroes, only: [:index, :show] 
+  resources :powers, only: [:index, :show, :update] 
+  resources :hero_powers, only: [:index, :create] 
   
-end
+
+ 
